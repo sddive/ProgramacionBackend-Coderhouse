9cd85fe1261a7046731319b2c3b068ea36ccff2c
Sergio Divenuto <sergiodivenuto@gmail.com>
2024-01-03 22:48:44 -0300

    strategy local

--- Entrega 09 - Refactor a nuestro login/src/config/config.passport.js
+++ Entrega 09 - Refactor a nuestro login/src/config/config.passport.js
@@ -1,8 +1,88 @@
 import passport from 'passport'
+import local from 'passport-local'
 import github from 'passport-github2'
 import { userModel } from '../dao/models/user.model.js'
+import { createHash, validPassword } from '../utils.js';
+import mongoose from "mongoose"
 
 export const initPassport=()=>{
+    passport.use('signup', new local.Strategy(
+        {
+            passReqToCallback: true, usernameField: 'email' //, passwordField: "clave"
+        },
+        async(req, username, password, done)=>{
+            try {
+                let {name, email} = req.body
+                if(!name || !email || !password){
+                    return done(null, false)
+                }
+            
+                let regMail=/^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/
+                console.log(regMail.test(email))
+                if(!regMail.test(email)){
+                    return done(null, false)
+                }
+            
+                let existe = await userModel.findOne({email})
+                if(existe){
+                    return done(null, false)
+                }
+
+                password = createHash(password)
+                let user
+                try {
+                    user = await userModel.create({name, email, password})
+                    // res.redirect(`/login?mensaje=Usuario ${email} registrado correctamente`)
+                    return done(null, user)
+                    // previo a devolver un usuario con done, passport graba en la req, una propiedad
+                    // user, con los datos del usuario. Luego podré hacer req.user
+                } catch (error) {
+                    // res.redirect('/registro?error=Error inesperado. Reintente en unos minutos')
+                    return done(null, false)
+                }
+                   
+            } catch (error) {
+                return done(error)
+            }
+        }
+    ))
+
+    passport.use('login', new local.Strategy(
+        {
+            usernameField: 'email'
+        },
+        async(username, password, done)=>{
+            try {
+                if(!username || !password){
+                    return done(null, false)
+                }
+                let user
+                if(username === 'adminCoder@coder.com' && password === 'adminCod3r123'){
+                    console.log('entre al admin')
+                    user = {_id: new mongoose.Types.ObjectId(), name:'Admin coder', email:'adminCoder@coder.com', role: 'admin'}
+                    return done(null, user)
+                }
+
+                user = await userModel.findOne({email:username}).lean()
+                if(!user){
+                    return done(null, false)
+                }
+                if(!validPassword(user, password)){
+                    return done(null, false)
+                }     
+
+                //console.log(Object.keys(user))
+                delete user.password
+                return done(null, user)
+                    // previo a devolver un usuario con done, passport graba en la req, una propiedad
+                    // user, con los datos del usuario. Luego podré hacer req.user
+
+            } catch (error) {
+                done(error, null)
+            }
+        }
+    ))    
+
 
     passport.use('github', new github.Strategy(
         {
@@ -24,7 +104,6 @@ export const initPassport=()=>{
                     usuario=await userModel.create(newUser)
                 }
                 return done(null, usuario)
-
 
             } catch (error) {
                 return done(error)
--- Entrega 09 - Refactor a nuestro login/src/routers/sessions.router.js
+++ Entrega 09 - Refactor a nuestro login/src/routers/sessions.router.js
@@ -1,72 +1,25 @@
 import { Router } from 'express'
 import __dirname from '../utils.js'
-import { userModel } from "../dao/models/user.model.js"
-import { createHash, validPassword } from '../utils.js';
 import passport from 'passport'
 
 const router = Router()
 
-router.post('/login', async(req, res)=>{
-
-    let {email, password} = req.body
-    if(!email || !password){
-        return res.redirect('/login?error=Complete todos los datos')
-    }
-
-    if(email === 'adminCoder@coder.com' && password === 'adminCod3r123'){
-        req.session.user = {
-            name:'Admin coder', email:'adminCoder@coder.com', role: 'admin'
-        }
-        return res.redirect('/products')
-    }
-
-    let user = await userModel.findOne({email})
-    if(!user){
-        return res.redirect(`/login?error=credenciales incorrectas`)
-    }
-    if(!validPassword(user, password)){
-        return res.redirect(`/login?error=credenciales incorrectas`)
-    }
-    
+router.post('/login', passport.authenticate('login',{failureRedirect:'/login?error=credenciales incorrectas'}), async(req, res)=>{    
     req.session.user = {
-        name:user.name, email:user.email, role:user.role
+        name:req.user.name, email:req.user.email, role:req.user.role
     }
 
     res.redirect('/products')
-
 })
 
-router.post('/sigup',async(req,res)=>{
-
-    let {name, email, password}=req.body
-    if(!name || !email || !password){
-        return res.redirect('/signup?error=Complete todos los datos')
+router.post('/signup', passport.authenticate('signup',{failureRedirect:'/signup?error=error al registrar, reintente nuevamente'}), async(req, res)=>{    
+    req.session.user = {
+        name:req.user.name, email:req.user.email, role:req.user.role
     }
-
-    let regMail=/^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/
-    console.log(regMail.test(email))
-    if(!regMail.test(email)){
-        return res.redirect('/signup?error=Mail con formato incorrecto...!!!')
-    }
-
-    let existe = await userModel.findOne({email})
-    if(existe){
-        return res.redirect(`/signup?error=Existen usuarios con email ${email} en la BD`)
-    }
-    
-    password = createHash(password)
-    try {
-        const user = await userModel.create({name, email, password})
-        res.redirect(`/login?mensaje=Usuario ${email} registrado correctamente`)
-        
-    } catch (error) {
-        res.redirect('/signup?error=Error inesperado. Reintente en unos minutos')
-    }
-
+    res.redirect(`/login?mensaje=Usuario ${req.user.email} registrado correctamente`)
 })
 
-router.get('/logout',(req,res)=>{
-    
+router.get('/logout',(req,res)=>{    
     req.session.destroy(error=>{
         if(error){
             res.redirect('/login?error=fallo en el logout')
